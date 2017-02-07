fe436a86cf1ec883551e104efe787f90110dad31
hejian65644955 <m15895420701@163.com>
2017-02-07 08:39:42 +0800

    新闻详情页面(NewsMenuDetailPager)的填充

--- app/src/main/java/com/atguigu/www/beijingnews/details/NewsMenuDetailPager.java
+++ app/src/main/java/com/atguigu/www/beijingnews/details/NewsMenuDetailPager.java
@@ -1,37 +1,90 @@
 package com.atguigu.www.beijingnews.details;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.view.Gravity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.widget.TextView;
+import android.view.ViewGroup;
 
+import com.atguigu.www.beijingnews.R;
 import com.atguigu.www.beijingnews.base.basepager.MenuDetailBasePager;
+import com.atguigu.www.beijingnews.bean.NewsCenterBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by lenovo on 2017/2/6.
  */
 
 public class NewsMenuDetailPager extends MenuDetailBasePager {
-    public NewsMenuDetailPager(Context mContext) {
+
+    /**
+     * 新闻详情页面的数据
+     */
+    private final List<NewsCenterBean.DataBean.ChildrenBean> childrenData;
+    /**
+     * 页签页面的集合
+     */
+    private ArrayList<TabDetailPager> tabDetailPagers;
+    @InjectView(R.id.viewpager)
+    ViewPager viewpager;
+
+    public NewsMenuDetailPager(Context mContext, NewsCenterBean.DataBean dataBean) {
         super(mContext);
+        this.childrenData = dataBean.getChildren();
     }
 
-    private TextView textView;
 
     @Override
     public View initView() {
         //新闻详细页面的视图
-        textView = new TextView(mContext);
-        textView.setTextSize(20);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(mContext, R.layout.new_menu_detail_pager, null);
+        ButterKnife.inject(this,view);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText("新闻详情页面内容");
+
+        //准备数据-页面
+        tabDetailPagers = new ArrayList<>();
+        for(int i = 0;i<childrenData.size();i++){
+            tabDetailPagers.add(new TabDetailPager(mContext,childrenData.get(i)));
+        }
+
+        //设置适配器
+        viewpager.setAdapter(new MyPagerAdapter());
+
+    }
+    class MyPagerAdapter extends PagerAdapter{
+
+        @Override
+        public int getCount() {
+            return tabDetailPagers.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view ==object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            TabDetailPager tabDetailPager = tabDetailPagers.get(position);
+            tabDetailPager.initData();
+            View rootView = tabDetailPager.rootView;
+            container.addView(rootView);
+            return  rootView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
     }
 }
---
+++ app/src/main/java/com/atguigu/www/beijingnews/details/TabDetailPager.java
@@ -0,0 +1,42 @@
+package com.atguigu.www.beijingnews.details;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.atguigu.www.beijingnews.base.basepager.MenuDetailBasePager;
+import com.atguigu.www.beijingnews.bean.NewsCenterBean;
+
+/**
+ * Created by lenovo on 2017/2/6.
+ */
+
+public class TabDetailPager extends MenuDetailBasePager {
+
+    private final NewsCenterBean.DataBean.ChildrenBean childrenBean;
+
+    public TabDetailPager(Context mContext, NewsCenterBean.DataBean.ChildrenBean childrenBean) {
+        super(mContext);
+        this.childrenBean = childrenBean;
+    }
+
+    private TextView textView;
+
+    @Override
+    public View initView() {
+        //图组详细页面的视图
+        textView = new TextView(mContext);
+        textView.setTextSize(20);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextColor(Color.RED);
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        textView.setText(childrenBean.getTitle());
+    }
+}
